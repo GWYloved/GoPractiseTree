b581f02b02cfd7d25a7754e45d9229cc07add40a
GWYloved <913925047@qq.com>
2018-03-19 19:01:56 +0800

    practise echo

--- echo/echodemo.go
+++ echo/echodemo.go
@@ -4,7 +4,10 @@ import (
 	"net/http"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"database/sql"
+	"fmt"
+	"os"
+	"io"
+	"syscall"
 )
 
 func main()  {
@@ -17,6 +20,11 @@ func main()  {
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+
+	e.POST("/save", save)
+	e.POST("/saveAvator", saveAvator)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -24,4 +32,53 @@ func main()  {
 }
 func hello(c echo.Context) error  {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+//for /users/:id
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	fmt.Println(id)
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team: "+team +", member: "+member+"\n")
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+}
+
+func saveAvator(c echo.Context) error {
+	name := c.QueryParam("name")
+	avator, err := c.FormFile("avator")
+	if err != nil {
+		return err
+	}
+	fmt.Printf(avator.Filename)
+	src, err := avator.Open()
+	checkNotNil(err)
+	defer src.Close()
+
+	dst, err := os.Create(avator.Filename)
+	checkNotNil(err)
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+	return c.HTML(http.StatusOK, "<b>Thank you ! "+name +"</b>")
+}
+
+func checkNotNil(err error) error {
+	if err!= nil {
+		 return err
+	}
+	syscall.Exit(101)
+	return nil
 }
